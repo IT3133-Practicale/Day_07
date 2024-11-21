b75f516faf39b394771951e2e6c6cbd505dd3fdc
PasinduNuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2024-11-21 21:09:51 +0530

    Rievive from child

--- day_07/src/App.js
+++ day_07/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react'
+import Child from './Components/Child'
 
 function App() {
   const [childName,setChildName]-useState("Default");
@@ -10,6 +11,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello, I'm Parent</h1>
+      <Child setfun={receiveName}></Child>
+      <p>Parent: My child said: {childName}</p>
     </div>
   );
 }
